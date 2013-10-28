d30ab7135cef3cb98e92b780e81903d543e96a9f
holiness992@gmail.com <holiness992@gmail.com>
2013-10-28 17:53:07 +0800

    删除无用代码

--- LibraryManagement/LibraryManagement/FrmBookPressManager.cs
+++ LibraryManagement/LibraryManagement/FrmBookPressManager.cs
@@ -18,95 +18,19 @@ namespace LibraryManagement
 
         private void FrmBookconcernManager_Load(object sender, EventArgs e)
         {
-            aaa();
+            
         }
 
-        private void aaa()
-        {
-            lvBookconcern.Items.Clear();
-            string sql = string.Format("select * from Presses order by upper(PressName)");
-            try
-            {
-                SqlCommand com = new SqlCommand(sql, DBHelper.conn);
-                DBHelper.conn.Open();
-                SqlDataReader dr = com.ExecuteReader();
-                ListViewItem item = null;
-                while (dr.Read())
-                {
-
-                    string name = dr["PressName"].ToString();
-                    item = new ListViewItem(name);
-                    lvBookconcern.Items.Add(item);
-
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-
-            }
-            finally
-            {
-                DBHelper.conn.Close();
-            }
-        }
+       
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if ( labelTextBox1.Text == "")
-            {
-                aaa();
-            }
-            else
-            {
-
-                lvBookconcern.Items.Clear();
-                string sql = string.Format("select * from Presses where PressName like '%{0}%'", labelTextBox1.Text);
-                try
-                {
-                    DBHelper.conn.Open();
-                    SqlCommand com = new SqlCommand(sql, DBHelper.conn);
-                    SqlDataReader dr = com.ExecuteReader();
-                    if (!dr.HasRows)
-                    {
-                        MessageBox.Show("没有该出版社，请自行添加！", "提示", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                    }
-                    else
-                    {
-                        while (dr.Read())
-                        {
-                            string name1 = dr["PressName"].ToString();
-                            ListViewItem item = new ListViewItem(name1);
-                            
-                            lvBookconcern.Items.Add(item);
-                        }
-
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-                finally
-                {
-                    DBHelper.conn.Close();
-                }
-            }
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (lvBookconcern.SelectedIndices!= null && lvBookconcern.SelectedIndices.Count > 0)
-            {
-                DBHelper.name = lvBookconcern.Items[0].Text;
-                this.Close();
-
-
-            }
-            else
-            {
-                MessageBox.Show("请选择出版社！");
-            }
+           
         }
 
         private void lvBookconcern_SelectedIndexChanged(object sender, EventArgs e)
