55c98e94801ee37145e09e66751518adcece18d6
Diluka <pmars42@gmail.com>
2013-11-01 11:23:36 +0800

    借书

---
+++ LibrarySystem/LibraryDatabase/dbo/Views/OrderView.sql
@@ -0,0 +1,14 @@
+﻿CREATE VIEW [dbo].[OrderView]
+	AS 
+	SELECT 
+		Orders.OrderID AS 订单号,
+		Orders.BookID AS 书本编号,
+		Orders.UID AS 用户ID,
+		BookInfo.Title AS 标题,
+		Authors.AuthorName AS 作者,
+		Orders.LeaseDate AS 借出日期
+	FROM 
+		Orders 
+		JOIN Books ON Orders.BookID=Books.BookID
+		JOIN BookInfo ON BookInfo.InfoID=Books.InfoID
+		JOIN Authors ON Authors.AuthorID=BookInfo.AuthorID
