a7b05b36cba036dc96359d20f1d605cdbd38699c
Diluka <pmars42@gmail.com>
2013-11-01 15:22:06 +0800

    又是删除问题，其实完全不是这样的，根本不应该删除才是，不管了，以后不能这样设计

--- LibrarySystem/LibraryDatabase/dbo/Stored Procedures/proc_del_book.sql
+++ LibrarySystem/LibraryDatabase/dbo/Stored Procedures/proc_del_book.sql
@@ -2,9 +2,11 @@
 as
 	if (select IsLeased from Books where BookID=@bid)=0
 	begin
-		update BookInfo set Total-=1,Remain-=1 where InfoID=(select InfoID from Books where BookID=@bid)
+		update BookInfo set Total-=1,Remain-=1 where InfoID=(select InfoID from Books where BookID=@bid);
 
-		delete Books where BookID=@bid
+		delete Records where BookID=@bid;
+		delete Orders where BookID=@bid;
+		delete Books where BookID=@bid;
 
 		return 1;
 	end
